d3b7ece6775fc561b70091fdf56a1e080f55d60f
David Omar Flores Chávez <david@davidomar.com>
2020-06-14 03:12:27 -0500

    Add some initial styles and buttons to disable flags

--- src/RegexInput.tsx
+++ src/RegexInput.tsx
@@ -1,29 +1,67 @@
-import React, { ChangeEvent } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 type RegexInputProps = {
   setRegex: Function;
 };
 
 function RegexInput({ setRegex }: RegexInputProps) {
-  const handleInputChange = (e: ChangeEvent) => {
-    const value = (e as ChangeEvent<HTMLInputElement>).target.value;
-    if (value === "") {
-      setRegex((null as unknown) as RegExp);
-    } else {
-      try {
-        const regex = new RegExp(`${value}`, "g");
-        setRegex(regex);
-      } catch {}
-    }
-  };
+  const handleSetRegex = useCallback(
+    (userRegex: string, flags: { g: boolean; i: boolean; m: boolean }) => {
+      if (userRegex === "") {
+        setRegex((null as unknown) as RegExp);
+      } else {
+        try {
+          let flagsString = "";
+          for (const flag in flags) {
+            if (flags.hasOwnProperty(flag)) {
+              if (flags[flag as "g" | "i" | "m"]) {
+                flagsString += flag;
+              }
+            }
+          }
+          const regex = new RegExp(`${userRegex}`, flagsString);
+          setRegex(regex);
+        } catch {}
+      }
+    },
+    [setRegex]
+  );
+
+  const [flags, setFlags] = useState({ g: true, i: true, m: true });
+  const [userRegex, setUserRegex] = useState("");
+
+  useEffect(() => {
+    handleSetRegex(userRegex, flags);
+  }, [flags, userRegex, handleSetRegex]);
 
   return (
-    <div>
-      <form>
-        <label>Your regex:</label>
-        <input onChange={handleInputChange}></input>
-      </form>
-    </div>
+    <form onSubmit={(event) => event.preventDefault()}>
+      <label>Your regex:</label>
+      {"     "}/
+      <input
+        type="text"
+        onChange={(event) => setUserRegex(event.target.value)}
+      ></input>
+      /
+      <button
+        className={flags.g ? "active" : "inactive"}
+        onClick={() => setFlags({ ...flags, g: !flags.g })}
+      >
+        g
+      </button>
+      <button
+        className={flags.i ? "active" : "inactive"}
+        onClick={() => setFlags({ ...flags, i: !flags.i })}
+      >
+        i
+      </button>
+      <button
+        className={flags.m ? "active" : "inactive"}
+        onClick={() => setFlags({ ...flags, m: !flags.m })}
+      >
+        m
+      </button>
+    </form>
   );
 }
 
--- src/RegexTests.tsx
+++ src/RegexTests.tsx
@@ -7,39 +7,58 @@ type RegexTestProps = {
 };
 
 function RegexTests({ regex }: RegexTestProps) {
-  const separateTests = (test: string) => {
-    if (test.match(regex)) {
-      const nonMatched = test.split(regex);
-      const matched = test.match(regex);
+  const separateTests = (test: { content: string; shouldMatch: boolean }) => {
+    if (test.content.match(regex)) {
+      const nonMatched = test.content.split(regex);
+      const matched = test.content.match(regex);
       let result = [{ text: nonMatched[0], matched: false }];
       for (let i = 1; i < nonMatched.length; i++) {
         result.push({ text: matched![i - 1], matched: true });
         result.push({ text: nonMatched[i], matched: false });
       }
-      return result;
+      const success = test.shouldMatch
+        ? nonMatched.filter((e) => e !== "").length === 0
+        : matched?.length === 0;
+      return {
+        content: result,
+        success
+      };
     }
 
-    return [{ text: test, matched: false }];
+    return {
+      content: [{ text: test.content, matched: false }],
+      success: false
+    };
   };
 
   const tests = [
-    "boomer",
-    "zoomer",
-    "coomer",
-    "the consoomer",
-    "migueloomert"
-  ].map((e) => ({ id: e, content: separateTests(e) }));
+    { content: "I cannot stand boomers", shouldMatch: true },
+    { content: "I'm such a zoomer, you know?", shouldMatch: true },
+    { content: "The consoomer is a boomer", shouldMatch: true },
+    { content: "I hate migueloomert the cheater", shouldMatch: false }
+  ].map((e) => ({
+    id: e.content,
+    shouldMatch: e.shouldMatch,
+    ...separateTests(e)
+  }));
 
   return (
-    <div>
+    <>
       {tests.map((test) => (
-        <li key={test.id}>
+        <div
+          className={
+            "regex-test" +
+            (test.success ? " successful" : "") +
+            (test.shouldMatch ? " matcheable" : " non-matcheable")
+          }
+          key={test.id.replace(/\s/, "")}
+        >
           {test.content.map((e) => (
             <p className={e.matched ? "matched" : ""}>{e.text}</p>
           ))}
-        </li>
+        </div>
       ))}
-    </div>
+    </>
   );
 }
 
