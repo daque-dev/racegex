eb469cbb026886a0af81b8e83b59fa5816f6b3df
David Omar Flores Chávez <david@davidomar.com>
2020-07-11 15:24:53 -0500

    Remove Fragment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
-import React, { Fragment } from 'react';
+import React from 'react';
 
 export default function Home() {
   return (
-    <Fragment>
+    <>
       <Head>
         <title>Next.js + Jest + TypeScript</title>
       </Head>
       <p>A simple example repo</p>
-    </Fragment>
+    </>
   );
 }
