02f18391b1868f6c21f7f0a184cf62a049be1823
David Omar Flores Chávez <david@davidomar.com>
2020-07-12 16:17:49 -0500

    Use previous Homepage using Typical

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import classnames from 'classnames/bind';
+// @ts-ignore
+import Typical from 'react-typical';
 
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
+const cx = classnames.bind(styles);
+
 export default function Home() {
+  let copy = `Whether for pleasure, curiosity, or necessity, we'll help you learn regular expressions.`;
+  const [regexes] = useState(
+    [
+      'whe\\S+',
+      'wh?e\\S*',
+      'regular',
+      '[a-z]+ity',
+      '[a-z]*e[a-z]*',
+      'erasure',
+      'er?asure',
+    ].reduce(
+      (acc: any[], cur: string) => [...acc, cur, () => onFinish(cur), 1500],
+      []
+    )
+  );
+
+  const [regex, setRegex] = useState('');
+
+  const onFinish = useCallback(setRegex, [setRegex]);
+
   return (
     <>
       <Head>
-        <title>Next.js + Jest + TypeScript</title>
+        <title>Racegex</title>
       </Head>
-      <p>A simple example repo</p>
+      <div className={cx('home')}>
+        <div className={cx('description')}>
+          <div className={cx('title', 'interactive')}>
+            /
+            <Typical loop={Infinity} steps={regexes} />
+            /gi
+          </div>
+          <div className={cx('subtitle')}>
+            {copy
+              .replace(new RegExp(`(${regex})`, 'gi'), '|$1|')
+              .split('|')
+              .map((e, i) =>
+                i % 2 === 0 ? e : <span key={`${e}-${i}`}>{e}</span>
+              )}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
