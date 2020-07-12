5fbdb3539949c2bae4207f6a538cfc3053745ea1
David Omar Flores Chávez <david@davidomar.com>
2020-07-12 16:13:58 -0500

    Update layout to include the Navbar in

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,15 @@
 import { PropsWithChildren } from 'react';
 
 import styles from './layout.module.scss';
+import NavBar from '@components/NavBar/NavBar';
 
 const Layout = ({ children }: PropsWithChildren<any>) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.pageContainer}>
+      <NavBar />
+      <div className={styles.contentContainer}>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
