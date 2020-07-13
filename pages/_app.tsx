5873aeb01c4439c682c6fa83e82a998b5e644b53
David Omar Flores Chávez <david@davidomar.com>
2020-07-12 21:09:08 -0500

    Show animations on route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,42 @@ import Layout from '@components/Layout/Layout';
 import Loader from '@components/Loader/Loader';
 
 import useLoader from '@hooks/use-loader';
+import { useRouter } from 'next/router';
+
+import { AnimatePresence, motion } from 'framer-motion';
+
+const easing = [0.6, -0.05, 0.01, 0.99];
 
 function MyApp({ Component, pageProps }: AppProps) {
   const isLoading = useLoader();
+  const router = useRouter();
 
-  return <Layout>{isLoading ? <Loader /> : <Component {...pageProps} />}</Layout>;
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Layout>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <motion.div
+            exit={{
+              opacity: 0,
+            }}
+            initial={{
+              y: 50,
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.6, ease: easing },
+            }}
+          >
+            <Component {...pageProps} key={router.route} />
+          </motion.div>
+        )}
+      </Layout>
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
